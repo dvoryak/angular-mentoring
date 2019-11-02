c314e2059c208053ae86b26bbe150e1aa4546267
Pavel Dvoriak <p.dvoryak@icloud.com>
2019-11-02 13:08:25 +0200

    Define constructors

--- src/app/entity/impl/course-model.ts
+++ src/app/entity/impl/course-model.ts
@@ -1,9 +1,17 @@
 import {ICourseModel} from '../course-model';
 
 export class CourseModel implements ICourseModel {
-    creationDate: Date;
-    description: string;
-    duration: number;
     id: number;
     title: string;
+    creationDate: Date;
+    duration: number;
+    description: string;
+
+    constructor(id: number, title: string, creationDate: Date, duration: number, description: string) {
+        this.id = id;
+        this.title = title;
+        this.creationDate = creationDate;
+        this.duration = duration;
+        this.description = description;
+    }
 }
--- src/app/entity/impl/user-model.ts
+++ src/app/entity/impl/user-model.ts
@@ -1,7 +1,13 @@
 import {IUserModel} from '../user-model';
 
 export class UserModel implements IUserModel {
-    firstName: string;
     id: number;
+    firstName: string;
     lastName: string;
+
+    constructor(id: number, firstName: string, lastName: string) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
