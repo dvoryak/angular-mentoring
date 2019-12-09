45b8180662b2c46f4423141c645401c34e353d4e
Pavel Dvoriak <p.dvoryak@icloud.com>
2019-12-09 14:44:22 +0200

    Use build in pipe for date format

--- src/app/shared/courses.constant.ts
+++ src/app/shared/courses.constant.ts
@@ -4,7 +4,7 @@ export const COURSES: CourseModel[] = [
     {
         id: 1,
         title: 'Video Course 1. Name tag',
-        creationDate: '9 Nov, 2018',
+        creationDate: new Date('2019-10-07'),
         duration: '87',
         // tslint:disable-next-line:max-line-length
         description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.'
@@ -12,7 +12,7 @@ export const COURSES: CourseModel[] = [
     {
         id: 2,
         title: 'Video Course 1. Name tag',
-        creationDate: '9 Nov, 2018',
+        creationDate: new Date('2018-05-02'),
         duration: '93',
         // tslint:disable-next-line:max-line-length
         description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.'
@@ -20,7 +20,7 @@ export const COURSES: CourseModel[] = [
     {
         id: 3,
         title: 'Video Course 1. Name tag',
-        creationDate: '9 Nov, 2018',
+        creationDate: new Date('2020-12-07'),
         duration: '123',
         // tslint:disable-next-line:max-line-length
         description: 'Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college\'s classes. They\'re published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.'
