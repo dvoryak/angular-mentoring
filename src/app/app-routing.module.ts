15b62d5cfe422eba470e314069070c006c487390
Pavlo Dvoriak <Pavlo_Dvoriak@epam.com>
2020-01-24 16:40:00 +0300

    Add basic setup to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {CourseComponent} from './components/course/course.component';
+import {NotFoundComponent} from './shared/not-found/not-found.component';
+import {CoursePageComponent} from './components/course-page/course-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    data: {
+      breadcrumb: 'Login'
+    }
+  },
+  {
+    path: 'courses',
+    component: CoursePageComponent,
+    data: {
+      breadcrumb: 'Courses'
+    }
+  },
+  {path: '', redirectTo: '/courses', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
