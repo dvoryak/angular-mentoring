6083066e44ffce002d3b8c11c99a49f38334d585
Pavlo Dvoriak <Pavlo_Dvoriak@epam.com>
2020-01-30 19:04:49 +0300

    Fix guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {NotFoundComponent} from './shared/not-found/not-found.component';
 import {CoursePageComponent} from './components/course-page/course-page.component';
 import {AddCoursePageComponent} from './components/add-course-page/add-course-page.component';
 import {EditCourseComponent} from './components/edit-course/edit-course.component';
+import {AuthorizationGuard} from './core/guards/authorization.guard';
 
 
 const routes: Routes = [
@@ -14,15 +15,18 @@ const routes: Routes = [
   },
   {
     path: 'courses',
-    component: CoursePageComponent
+    component: CoursePageComponent,
+    canActivate: [AuthorizationGuard]
   },
   {
     path: 'create-course',
-    component: AddCoursePageComponent
+    component: AddCoursePageComponent,
+    canActivate: [AuthorizationGuard]
   },
   {
     path: 'edit-course/:id',
-    component: EditCourseComponent
+    component: EditCourseComponent,
+    canActivate: [AuthorizationGuard]
   },
   {path: 'not-found', component: NotFoundComponent},
   {path: '', redirectTo: '/courses', pathMatch: 'full'},
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,7 +3,6 @@ import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 import {User} from '../../entity';
-import {tap} from "rxjs/operators";
 
 @Component({
     selector: 'app-header',
